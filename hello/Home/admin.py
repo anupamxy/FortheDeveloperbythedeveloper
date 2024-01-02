0a20e1611c133d3b95755917a1cfbc08f6eafb59
anupamxy <123785384+anupamxy@users.noreply.github.com>
2024-01-02 12:06:34 +0530

    final commmiting
    final committing for working content

---
+++ hello/Home/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from Home.models import Contact, Employee, MyModel, Position
+from Home.models import GeeksModel
+admin.site.register(Contact)#register your Models to
+admin.site.register(GeeksModel)
+admin.site.register(MyModel)
+
+
+
+# Register your models here.
